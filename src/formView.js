7b4f08c3b2b17bac5e2c93cac6963eaae4e2dc4d
Cainito <nikita.shakirzhanov@gmail.com>
2025-06-27 16:19:24 +0400

    app

--- src/formView.js
+++ src/formView.js
@@ -1,39 +1,50 @@
-import { state } from './index.js';
-
+const headerTextEl = document.querySelector('#header-text');
+const leadEl = document.querySelector('#lead');
+const sampleEl = document.querySelector('#sample');
+const btnAdd = document.querySelector('#button-add');
 const form = document.querySelector('form');
 const input = document.querySelector('#url-input');
-const inputFeedbackEl = document.querySelector('#invalid-url-alert');
+const label = document.querySelector('label');More actions
+const feedbackWrapperEl = document.querySelector('#feedback-wrapper');
 
-export const render = () => {
-  if (state.formState === 'filling') {
-    form.reset();
-    input.classList.remove('is-invalid');
-    inputFeedbackEl.innerText = '';
-  }
-  if (state.formState === 'invalid') {
-    inputFeedbackEl.classList.remove('text-success');
-    inputFeedbackEl.classList.add('text-danger');
-    input.classList.add('is-invalid');
-    inputFeedbackEl.innerText = 'Ссылка должна быть валидным URL';
-  }
-  if (state.formState === 'repeated_value') {
-    inputFeedbackEl.classList.remove('text-success');
-    inputFeedbackEl.classList.add('text-danger');
-    input.classList.add('is-invalid');
-    inputFeedbackEl.innerText = 'RSS уже существует';
-  }
-  if (state.formState === 'submitted') {
-    // This when rss file successfully downloaded
-    input.classList.remove('is-invalid');
-    inputFeedbackEl.classList.remove('text-danger');
-    inputFeedbackEl.classList.add('text-success');
-    inputFeedbackEl.innerText = 'RSS успешно загружен';
-  }
-};
+export const render = (watchedState) => {
+  label.innerText = watchedState.uiState.label;
+  headerTextEl.innerText = watchedState.uiState.header;
+  leadEl.innerText = watchedState.uiState.lead;
+  sampleEl.innerText = watchedState.uiState.sample;
+  btnAdd.value = watchedState.uiState.buttonText;
 
-export const addFormInputHandler = (handler) => {
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    handler(input.value);
-  });
+  if (watchedState.formState === 'filling') {
+    input.classList.remove('is-invalid');
+    feedbackWrapperEl.innerHTML = `More actions
+    <p id="feedback" class="feedback m-0 position-absolute small">
+      ${watchedState.uiState.feedback}
+    </p>`;
+  }
+
+  if (watchedState.formState === 'invalid') {
+    input.classList.add('is-invalid');
+    feedbackWrapperEl.innerHTML = `More actions
+      <p id="feedback" class="feedback m-0 position-absolute small text-danger">
+      ${watchedState.uiState.feedback}
+      </p>`;
+  }
+
+  if (watchedState.formState === 'not_unique') {
+    input.classList.add('is-invalid');
+    feedbackWrapperEl.innerHTML = `More actions
+    <p id="feedback" class="feedback m-0 position-absolute small text-danger">
+    ${watchedState.uiState.feedback}
+    </p>`;
+  }
+
+  if (watchedState.formState === 'submitted') {
+    input.classList.remove('is-invalid');
+    feedbackWrapperEl.innerHTML = `More actions
+    <p id="feedback" 
+      class="feedback m-0 position-absolute small text-success">
+      ${watchedState.uiState.feedback}
+    </p>`;
+    input.value = '';
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as yup from 'yup';
-import onChange from 'on-change';
-import { addFormInputHandler, render } from './formView';
+import app from './app.js';
 
-export const state = {
-  links: [],
-  formState: 'filling',
-};
-
-const watchedState = onChange(state, render);
-
-const handleFormInput = (value) => {
-  inputSchema
-    .validate(value)
-    .then((value) => {
-      watchedState.links.push(value);
-      watchedState.formState = 'submitted'; // it should be downloaded first
-    })
-    .catch((err) => {
-      if (err.message === 'invalid URL') watchedState.formState = 'invalid';
-      if (err.message === 'already added') watchedState.formState = 'repeated_value';
-      console.error(err);
-    });
-};
-
-addFormInputHandler(handleFormInput);
-
-render();
-const inputSchema = yup
-  .string()
-  .trim()
-  .url('invalid URL')
-  .test('is unique', 'already added', (url) => !state.links.includes(url));
+app();
